e2651348baa4abb06ac6f98ff3ecf81bee49ab7b
Wang Zuo <wzuoadjusted@gmail.com>
2015-08-31 09:58:24 +0800

    update example

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
+var fs = require('fs');
 var progress = require('./');
 var request = require('request');
 
-request('http://cachefly.cachefly.net/10mb.test').pipe(progress(':bar'));
+request('http://cachefly.cachefly.net/10mb.test')
+  .pipe(progress(':bar'))
+  .pipe(fs.createWriteStream('10mb.test'));
