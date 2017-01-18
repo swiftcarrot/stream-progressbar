25eebb6346e8b7b8230e08dc455c2c1c54b99988
Wang Zuo <wzuoadjusted@gmail.com>
2017-01-18 16:04:41 +0800

    lint fix

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var PassThrough = require('stream').PassThrough;
 var ProgressBar = require('progress');
 
-module.exports = function(tokens, options) {
+module.exports = function (tokens, options) {
   options = options || {};
 
   var pt = new PassThrough();
   var total = options.total;
 
-  pt.on('pipe', function(stream) {
-    if(total) {
+  pt.on('pipe', function (stream) {
+    if (total) {
       var bar = new ProgressBar(tokens, options);
-      pt.on('data', function(chunk) {
+      pt.on('data', function (chunk) {
         bar.tick(chunk.length);
       });
     } else {
-      stream.on('response', function(res) {
+      stream.on('response', function (res) {
         var total = parseInt(res.headers['content-length'], 10);
         options.total = total;
         var bar = new ProgressBar(tokens, options);
 
-        pt.on('data', function(chunk) {
+        pt.on('data', function (chunk) {
           bar.tick(chunk.length);
         });
       });
